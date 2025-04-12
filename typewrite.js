5543a930aaa972f772ad63a1acf4c13d931fd6bc
Ian Chang <ianachang88@gmail.com>
2025-04-11 19:29:13 -0700

    DRY

--- typewrite.js
+++ typewrite.js
@@ -1,4 +1,5 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const addCursor = (txt) => `<span class="wrap">${txt}</span>`;
 
 class Typewrite {
   constructor(element, finalText) {
@@ -9,9 +10,9 @@ class Typewrite {
 
   async blink() {
     while (true) {
-      this.element.innerHTML = `<span class="wrap">${this.finalText}</span>`;
+      this.element.innerHTML = addCursor(this.finalText);
       await sleep(600);
-      this.element.innerHTML = this.finalText;
+      this.element.innerHTML = this.finalText; // Undo cursor
       await sleep(600);
     }
   }
@@ -20,7 +21,7 @@ class Typewrite {
     let currentText = "";
     for (const char of this.finalText) {
       currentText += char;
-      this.element.innerHTML = `<span class="wrap">${currentText}</span>`;
+      this.element.innerHTML = addCursor(currentText);
       await sleep(250);
     }
     await this.blink();
