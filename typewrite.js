c7e12d5950f36e39141eed218d535783daec03e3
Ian Chang <ianachang88@gmail.com>
2025-04-11 19:27:02 -0700

    Prettier format

--- typewrite.js
+++ typewrite.js
@@ -1,35 +1,35 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 class Typewrite {
-    constructor(element, finalText) {
-        this.element = element;
-        this.finalText = finalText;
-        this.type();
-    }
+  constructor(element, finalText) {
+    this.element = element;
+    this.finalText = finalText;
+    this.type();
+  }
 
-    async blink() {
-        while (true) {
-            this.element.innerHTML = `<span class="wrap">${this.finalText}</span>`;
-            await sleep(600);
-            this.element.innerHTML = this.finalText;
-            await sleep(600);
-        }
+  async blink() {
+    while (true) {
+      this.element.innerHTML = `<span class="wrap">${this.finalText}</span>`;
+      await sleep(600);
+      this.element.innerHTML = this.finalText;
+      await sleep(600);
     }
-    
-    async type() {
-        let currentText = '';
-        for (const char of this.finalText) {
-            currentText += char;
-            this.element.innerHTML = `<span class="wrap">${currentText}</span>`;
-            await sleep(250);
-        }
-        await this.blink();
+  }
+
+  async type() {
+    let currentText = "";
+    for (const char of this.finalText) {
+      currentText += char;
+      this.element.innerHTML = `<span class="wrap">${currentText}</span>`;
+      await sleep(250);
     }
+    await this.blink();
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const element = document.getElementsByClassName('typewrite');
-    if (element.length) { 
-        new Typewrite(element[0], element[0].textContent); 
-    }
-}); 
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementsByClassName("typewrite");
+  if (element.length) {
+    new Typewrite(element[0], element[0].textContent);
+  }
+});
